7ea0c54dea79da58553341c4a321360a6166bba9
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-07 19:23:22 +0530

    Create variables.tf

---
+++ modules/LoadBalancer/variables.tf
@@ -0,0 +1,11 @@
+variable "lb_name" {
+  description = "Name of the Load Balancer"
+}
+
+variable "internal" {
+  description = "Internal or External Load Balancer"
+}
+
+variable "lb_type" {
+  description = "Type of Load Balancer"
+}
