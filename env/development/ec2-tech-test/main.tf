9738b2252823eb025182e3de7e36406524d0dce6
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-12-07 19:35:52 +0530

    Update main.tf

--- env/development/ec2-tech-test/main.tf
+++ env/development/ec2-tech-test/main.tf
@@ -10,3 +10,21 @@ module "load_balancer" {
   internal = false
   lb_type  = "application"
 }
+
+resource "aws_instance" "ec2_instance" {
+  ami           = module.ec2_instance.ami
+  instance_type = module.ec2_instance.instance_type
+
+  # Add EC2 instance to the Load Balancer target group
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [module.load_balancer]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws elbv2 register-targets --target-group-arn ${module.load_balancer.lb_target_group_arn} --targets Id=${aws_instance.ec2_instance.id}
+    EOT
+  }
+}
